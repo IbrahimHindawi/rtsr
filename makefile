39270c2261591d3e956e94fcd12e607fc1615f31
ibra <ihindawi@gmail.com>
2021-05-20 01:53:24 +0400

    integrated SDL2 into C build

--- makefile
+++ makefile
@@ -1,5 +1,14 @@
 build:
-	gcc -Wall -std=c89 ./src/main.c -o bin/cprog
+	gcc -Wall \
+	-Wfatal-errors \
+	-std=c89 \
+	./src/main.c \
+	-I"C:\libsdl\include" \
+	-L"C:\libsdl\lib" \
+	-lmingw32 \
+	-lSDL2main \
+	-lSDL2 \
+	-o bin/cprog
 
 run:	
 	./bin/cprog
--- src/main.c
+++ src/main.c
@@ -1,7 +1,18 @@
+/*
 #include <stdio.h>
+#include <SDL2/SDL.h>
 
 int main(void) {
     const char* string = "Hell World!";
     printf("%s\n", string);
+    return 0;
+}
+*/
+
+#include <SDL2/SDL.h>
+
+int main(int argc, char *argv[]) {
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Quit();
     return 0;
 }
