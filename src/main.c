5cbac9e438ed2ae09c84d267289eedbc5abcad58
__ibrahim__ <ibrahim.hindawi@robocomvr.com>
2021-05-21 14:56:16 +0400

    begin

--- src/main.c
+++ src/main.c
@@ -1,18 +1,30 @@
-/*
+#include <SDL2/SDL.h>
 #include <stdio.h>
-#include <SDL2/SDL.h>
+#include <stdbool.h>
 
-int main(void) {
-    const char* string = "Hell World!";
-    printf("%s\n", string);
-    return 0;
+#define SDL_error -1
+
+static SDL_Window* window;
+
+bool initialize_window(void) {
+    int result =  SDL_Init(SDL_INIT_EVERYTHING);
+    if (result == SDL_error) {
+        char *error_string = "";
+        printf("%s\n", SDL_GetErrorMsg(error_string, 256));
+        return false;
+    }
+    window = SDL_CreateWindow("RTSR", 
+                    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                    800, 640, 0);
+    return true;
 }
-*/
-
-#include <SDL2/SDL.h>
 
 int main(int argc, char *argv[]) {
-    SDL_Init(SDL_INIT_EVERYTHING);
+    
+    initialize_window();
+    
+    printf("%I64u\n", sizeof(int));
+
     SDL_Quit();
     return 0;
 }
--- src/main.nim
+++ src/main.nim
@@ -1,4 +1,10 @@
 import sdl2
 
-echo "Hell World!"
-
+block:
+  var result = init(INIT_EVERYTHING)
+  if result == SdlError.toBool:
+    echo getError()
+    quit(-1)
+var window = createWindow("RTSR", 
+            SDL_WINDOWPOS_UNDEFINED.int32,SDL_WINDOWPOS_UNDEFINED,
+            800, 640, 0)
