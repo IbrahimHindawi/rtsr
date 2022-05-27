c676218db0f7da5f4c301690f5107b1a3b27eac9
__ibrahim__ <ibrahim.hindawi@robocomvr.com>
2022-05-27 06:51:14 +0400

    reorganized repo

--- src/mesh.h
+++ src/mesh.h
@@ -5,29 +5,9 @@
 #include "triangle.h"
 
 #define npts 8
-vec3_t verts[npts] = {
-	{-1.0f, -1.0f, -1.0f}, //0
-	{-1.0f,  1.0f, -0.0f}, //1
-	{ 1.0f,  1.0f, -0.0f}, //2
-	{ 1.0f, -1.0f, -0.0f}, //3
-	{ 1.0f,  1.0f,  0.0f}, //4
-	{ 1.0f, -1.0f,  0.0f}, //5
-	{-1.0f,  1.0f,  0.0f}, //6
-	{-1.0f, -1.0f,  0.0f}  //7
-};
+extern vec3_t verts[npts];
 
 #define nprim (6*2)
-face_t prims[nprim] = {
-	{1, 2, 3},
-	{1, 3, 4},
-	{4, 3, 5},
-	{4, 5, 6},
-	{6, 5, 7},
-	{6, 7, 8},
-	{2, 7, 5},
-	{2, 5, 3},
-	{6, 8, 1},
-	{6, 1, 4}
-};
+extern face_t prims[nprim];
 
 #endif
