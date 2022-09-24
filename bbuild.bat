f4c895077db73ecd7e80c11b6af0874a7de271e7
Ibrahim <ihindawi@gmail.com>
2022-09-24 10:30:53 +0400

    restructured build system

--- bbuild.bat
+++ bbuild.bat
@@ -1,1 +1,23 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\bin\Hostx86\x64\cl.exe" /DEBUG:FULL /Z7 src\main.c user32.lib gdi32.lib
+@echo off
+rem create build dir
+mkdir build
+
+rem go to build dir
+pushd build
+
+rem main exe name
+set project_name=main
+
+rem compiler path
+set cl_path="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\bin\Hostx86\x64"
+
+rem package manager path
+set vcpkg_path=%userprofile%\vcpkg\installed\x64-windows
+
+rem compiler command
+%cl_path%\cl.exe /nologo ..\src\%project_name%.c ..\src\display.c ..\src\salloc.c ..\src\vector.c /DEBUG:FULL /Z7 /I%vcpkg_path%\include user32.lib gdi32.lib %vcpkg_path%\lib\SDL2.lib
+
+rem add dynamic lib with exe
+XCOPY %vcpkg_path%\bin\SDL2.dll .
+
+popd
