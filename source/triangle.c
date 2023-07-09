ae863b0e28ca038b7d1fcf0cb3e7f6a6503742c4
x19 <ihindawi@gmail.com>
2023-07-09 10:51:49 +0400

    Update triangle.c

--- source/triangle.c
+++ source/triangle.c
@@ -1,4 +1,16 @@
 #include "triangle.h"
 
-
+/*
+void create_circle_vertices(vec3f *pts);
+{
+    float theta = 0.0f;
+    float step = (2 * PI * r) / npts;
+    int n = pts.length();
+    for(int i = 0; i < n; i++) {
+        pts[i]->x = sin(theta);
+        pts[i]->y = cos(theta);
+        theta += step;
+    }
+}
+*/
 
