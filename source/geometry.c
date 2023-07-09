14c7e5a6faddea53baf0bef9210f773d23bdc5a9
x19 <ihindawi@gmail.com>
2023-07-09 21:14:18 +0400

    Update geometry.c
    added circle index gen

--- source/geometry.c
+++ source/geometry.c
@@ -4,6 +4,7 @@
 #define PI 3.14159265359
 
 void create_circle_vertices(vec3_t *points, int n, float radius) {
+    // configure zero first
     float theta = 0.0f;
     float step = (2 * PI) / n;
     for(int i = 0; i < n; i++) {
@@ -14,3 +15,11 @@ void create_circle_vertices(vec3_t *points, int n, float radius) {
     }
 }
 
+void create_circle_indices(face_t *faces, int n) {
+    for(int i = 0; i < n; i++) {
+        faces[i].a = 0;
+        faces[i].b = i + 1;
+        faces[i].c = i + 2;
+    }
+    faces[n-1].c = 1;
+}
